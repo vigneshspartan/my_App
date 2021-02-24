a6a0cc567ae085d2a8efa31e928136c4e8fe76b0
Prakash-Sakthivel <Prakash160600@gmail.com>
2021-02-24 14:42:42 +0530

    commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton dairybutton;
     private ImageButton utensilsbutton;
     private ImageButton grocerybutton;
+    private Button final_check;
     public static String finallist = "";
     //request codes
 
@@ -32,18 +33,26 @@ public class MainActivity extends AppCompatActivity {
     public void onResume() {
         super.onResume();
         SharedPreferences sp = getSharedPreferences("infos", 0);
-        /*SharedPreferences.Editor ed = sp.edit();
+        SharedPreferences.Editor ed = sp.edit();
+        //ed.commit();
+        finallist+= sp.getString("veglist", "");
+        //sp.edit().clear().commit();
         ed.remove("veglist").commit();
+        finallist+=sp.getString("fruitslist","");
+        //sp.edit().clear().commit();
         ed.remove("fruitslist").commit();
+        finallist+=sp.getString("chocolatelist","");
+        //sp.edit().clear().commit();
         ed.remove("chocolatelist").commit();
+        finallist+= sp.getString("dairylist","");
         ed.remove("dairylist").commit();
-
-        ed.remove("fruitslist").commit();
+        //sp.edit().clear().commit();
+        finallist+=sp.getString("grocerylist","");
+        ed.remove("grocerylist").commit();
+        //sp.edit().clear().commit();
+        finallist+=sp.getString("utensilslist","");
         ed.remove("utensilslist").commit();
-        //ed.commit();*/
-        finallist+= sp.getString("veglist", "")+sp.getString("fruitslist","")+sp.getString("chocolatelist","")+sp.getString("dairylist","")+sp.getString("grocerylist","")+sp.getString("utensilslist","");
-        Log.d("temp", finallist);
-        sp.edit().clear().commit();
+        //sp.edit().clear().commit();
     }   
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,8 +65,17 @@ public class MainActivity extends AppCompatActivity {
         dairybutton = (ImageButton) findViewById(R.id.dairyButton);
         utensilsbutton = (ImageButton) findViewById(R.id.utensilsButton);
         grocerybutton = (ImageButton) findViewById(R.id.groceryButton);
+        final_check = (Button) findViewById(R.id.final_data_button);
+        final_check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("data_check", finallist);
+                SharedPreferences sp = getSharedPreferences("infos", 0);
+                finallist="";
+                sp.edit().clear().commit();
 
-
+            }
+        });
         fruitsbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
